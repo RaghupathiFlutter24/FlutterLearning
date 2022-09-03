0874e1ba298a347ac50fff9abf0676467c973f40
Raghu <raghupathy.g@doodleblue.com>
2022-09-03 10:15:57 +0530

    Add all my Files

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+class MyButton extends StatelessWidget{
+  const MyButton ({super.key});
+
+  @override
+    Widget build(BuildContext context){
+    return GestureDetector(
+      onTap: () {
+        print("Button Is Pressed");
+      },
+      child: Container(
+        height: 50.0,
+        padding: const EdgeInsets.all(8.0),
+        margin: const EdgeInsets.symmetric(horizontal: 8.0),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(5.0),
+          color: Colors.blue,
+        ),
+        child: const Center(
+          child: Text(
+            'Engage'
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+void main(){
+  runApp(
+    const MaterialApp(
+     home: Scaffold(
+       body: Center(
+         child: MyButton(),
+       ),
+     ),
+
+    ),
+  );
+}
